3bfa412cbbe4bbc3643e0877b0e0820de9aae994
Travja <the.only.t.craft@gmail.com>
2024-03-07 22:44:21 -0700

    Fix up POM

--- src/com/sucy/enchant/vanilla/Vanilla.java
+++ src/com/sucy/enchant/vanilla/Vanilla.java
@@ -12,34 +12,33 @@ import java.util.Optional;
  * com.sucy.enchant.vanilla.Vanilla
  */
 public class Vanilla {
-
-    public static final Optional<CustomEnchantment> getEnchantment(final String name) {
+    public static Optional<CustomEnchantment> getEnchantment(final String name) {
         final String conversion = ENCHANTS.getOrDefault(name.toLowerCase(), name).replace(" ", "_");
         return Optional.ofNullable(EnchantmentAPI.getEnchantment(conversion));
     }
 
-    private static final Map<String,String> ENCHANTS = new HashMap<String,String>() {{
-        put("knockback",             "KNOCKBACK");
-        put("looting",               "LOOT_BONUS_MOBS");
-        put("sharpness",             "DAMAGE_ALL");
-        put("smite",                 "DAMAGE_UNDEAD");
-        put("bane of arthropods",    "DAMAGE_ARTHROPODS");
-        put("fire aspect",           "FIRE_ASPECT");
-        put("infinity",              "ARROW_INFINITE");
-        put("flame",                 "ARROW_FIRE");
-        put("punch",                 "ARROW_KNOCKBACK");
-        put("power",                 "ARROW_DAMAGE");
-        put("respiration",           "OXYGEN");
-        put("aqua affinity",         "WATER_WORKER");
-        put("feather falling",       "PROTECTION_FALL");
-        put("thorns",                "THORNS");
-        put("protection",            "PROTECTION_ENVIRONMENTAL");
-        put("fire protection",       "PROTECTION_FIRE");
-        put("blast protection",      "PROTECTION_EXPLOSIONS");
+    private static final Map<String,String> ENCHANTS = new HashMap<>() {{
+        put("knockback", "KNOCKBACK");
+        put("looting", "LOOT_BONUS_MOBS");
+        put("sharpness", "DAMAGE_ALL");
+        put("smite", "DAMAGE_UNDEAD");
+        put("bane of arthropods", "DAMAGE_ARTHROPODS");
+        put("fire aspect", "FIRE_ASPECT");
+        put("infinity", "ARROW_INFINITE");
+        put("flame", "ARROW_FIRE");
+        put("punch", "ARROW_KNOCKBACK");
+        put("power", "ARROW_DAMAGE");
+        put("respiration", "OXYGEN");
+        put("aqua affinity", "WATER_WORKER");
+        put("feather falling", "PROTECTION_FALL");
+        put("thorns", "THORNS");
+        put("protection", "PROTECTION_ENVIRONMENTAL");
+        put("fire protection", "PROTECTION_FIRE");
+        put("blast protection", "PROTECTION_EXPLOSIONS");
         put("projectile protection", "PROTECTION_PROJECTILE");
-        put("efficiency",            "DIG_SPEED");
-        put("unbreaking",            "DURABILITY");
-        put("fortune",               "LOOT_BONUS_BLOCKS");
-        put("silk touch",            "SILK_TOUCH");
+        put("efficiency", "DIG_SPEED");
+        put("unbreaking", "DURABILITY");
+        put("fortune", "LOOT_BONUS_BLOCKS");
+        put("silk touch", "SILK_TOUCH");
     }};
 }
